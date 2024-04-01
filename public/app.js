b70547a5ebd2fa0aae4e7e337a1d0fb8cdf35e1f
Sumiran Pokharel <sumiran@Sumirans-MacBook-Pro.local>
2024-04-01 03:48:56 -0500

    Added hbcu db and darkmode

--- public/app.js
+++ public/app.js
@@ -46,5 +46,12 @@
         });
     });
     
-    
+// dark-mode/brightmode
+    var content = document.getElementsByTagName('body')[0];
+    var darkMode = document.getElementById('dark-change');
+    darkMode.addEventListener('click', function(){
+        darkMode.classList.toggle('active');
+        content.classList.toggle('night');
+    })
+  
 
